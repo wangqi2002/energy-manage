9ba32173198959079b3fe92cf38e01146d3b97a7
wangqi2002 <1722009706@qq.com>
2023-09-09 22:14:39 +0800

    folder finish 1.0

--- src/components/video/video.vue
+++ src/components/video/video.vue
@@ -1,25 +1,26 @@
 <template>
   <div class="video mt22">
-<!--    <iframe id="iframeV"  src="http://video.citgroup.cn/" frameborder="0" width="100%" ></iframe>-->
-    <video autoplay="autoplay" name="media" style="width:100%; object-fit: fill;border:1px solid #3b72ee;"><source src="../../../static/video/siemens.mp4" type="video/mp4"></video>
+    <video autoplay="autoplay" name="media" style="width:100%; object-fit: fill;border:1px solid #3b72ee;">
+      <source src="../../../static/video/industry.mp4" type="video/mp4">
+    </video>
   </div>
 </template>
 
 <script>
-  export default{
-    data(){
-      return{
+export default {
+  data() {
+    return {
 
-      }
-    },
-     mounted(){
-      this.$store.commit('UPDATEISLOGIN',true);
-    },
-  }
+    }
+  },
+  mounted() {
+    this.$store.commit('UPDATEISLOGIN', true);
+  },
+}
 
 </script>
 
 <style scoped>
-  #iframeV{height: 8.59rem}
-
-</style>
+#iframeV {
+  height: 8.59rem
+}</style>
